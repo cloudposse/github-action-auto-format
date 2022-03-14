44b79fee12abfbab1b05d085b114247445457fcb
Dylan Bannon <dylanbannon5@gmail.com>
2022-03-14 11:19:56 -0700

    debugging

--- terraform.sh
+++ terraform.sh
@@ -9,7 +9,7 @@ if [ -n "$output" ]; then
   echo "Changes detected. Pushing to the PR branch"
   git config --global user.name 'cloudpossebot'
   git config --global user.email '11232728+cloudpossebot@users.noreply.github.com'
-  git add -A -- ':!${IGNORE_PATH}' ':!github-action-terraform-ci'
+  git add -A -- ':!'"${IGNORE_PATH}"'' ':!github-action-terraform-ci'
   git commit -m "Auto Format"
   # Prevent looping by not pushing changes in response to changes from cloudpossebot
   [[ $SENDER ==  "cloudpossebot" ]] || git push
