c3618952717b20c16d2b1ab29e359d15510b229e
Dylan Bannon <dylanbannon5@gmail.com>
2022-04-07 13:25:53 -0700

    debugging action runtime

--- github_format.sh
+++ github_format.sh
@@ -3,12 +3,13 @@
 set -x
 
 echo "Copying workflows from cloudposse/.github repo"
-git config --global user.name 'cloudpossebot'
-git config --global user.email '11232728+cloudpossebot@users.noreply.github.com'
+#git config --global user.name 'cloudpossebot'
+#git config --global user.email '11232728+cloudpossebot@users.noreply.github.com'
 
 mkdir gha_tmp_dir
 cd gha_tmp_dir
-git clone https://.:${SPECIAL_TOKEN}@github.com/cloudposse/.github
+#git clone https://.:${SPECIAL_TOKEN}@github.com/cloudposse/.github
+git clone https://github.com/cloudposse/.github
 cp ./.github/workflow-templates/*.yml ../.github/workflows/
 cd ..
 rm -rf ./gha_tmp_dir
