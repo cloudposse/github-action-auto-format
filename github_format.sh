66dd527d977d254f3877d2a5efcd54b7c34243ca
Dylan Bannon <dylanbannon5@gmail.com>
2022-04-07 15:00:05 -0700

    cleaning up github_format action

--- github_format.sh
+++ github_format.sh
@@ -5,20 +5,15 @@ set -x
 echo "Copying workflows from cloudposse/.github repo"
 git config --local user.name 'cloudpossebot'
 git config --local user.email '11232728+cloudpossebot@users.noreply.github.com'
-#git config --local user.name 'dylanbannon'
-#git config --local user.email 'dylanbannon5@gmail.com'
 
 mkdir gha_tmp_dir
 cd gha_tmp_dir
-#git clone https://.:${SPECIAL_TOKEN}@github.com/cloudposse/.github
 git clone https://github.com/cloudposse/.github
 cp ./.github/workflow-templates/*.yml ../.github/workflows/
 cd ..
 rm -rf ./gha_tmp_dir
 
-git remote -v
-git config --list --show-origin
-git add -A -- ':!'"${IGNORE_PATH}"'' ':!github-action-terraform-ci'
+git add -A -- ':!'"${IGNORE_PATH}"''
 #git add -A -- ':!'"${IGNORE_PATH}"'' ':!github-action-terraform-ci'
 git commit -m "Adding .github files"
 # Prevent looping by not pushing changes in response to changes from cloudpossebot
