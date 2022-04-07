22097c193d49c6a7d4ae73513c99b0930cb4de66
Dylan Bannon <dylanbannon5@gmail.com>
2022-04-07 15:13:49 -0700

    debugging action runtime

--- readme.sh
+++ readme.sh
@@ -4,17 +4,19 @@ make BUILD_HARNESS_PATH=/build-harness PACKAGES_PREFER_HOST=true -f /build-harne
 
 set -x
 
+# The `readme` build-harness target leaves some files lying around after it finishes. Since we might be running other targets after this, in separate GitHub Action steps, we don't want to leave these files lying there, waiting to return some permission error or something. That's why they get deleted, whether there were changes to `README.md` or not.
+
 output=$(git diff --name-only)
 if [ -n "$output" ]; then
   echo "Changes detected. Pushing to the PR branch"
   git config --global user.name 'cloudpossebot'
   git config --global user.email '11232728+cloudpossebot@users.noreply.github.com'
+  rm .build-harness/
+  rm -r docs/
   git add -A -- ':!'"${IGNORE_PATH}"'' ':!github-action-terraform-ci'
   git commit -m "Auto Format"
 else
   echo "No changes detected"
+  rm .build-harness/
+  rm -r docs/
 fi
-
-# The `readme` build-harness target leaves some files lying around after it finishes. Since we might be running other targets after this, in separate GitHub Action steps, we don't want to leave these files lying there, waiting to return some permission error or something.
-rm -r .build-harness/
-rm -r docs/
