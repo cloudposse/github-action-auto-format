75f4a3a18dd9d0152f54cb8d59678f7bbbfbb6fb
Dylan Bannon <dylanbannon5@gmail.com>
2022-04-07 23:55:50 -0700

    debugging action runtime

--- terraform_format.sh
+++ terraform_format.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 if [[ "$HOST_REPO" == "cloudposse/github-action-auto-format" ]]; then
-  cp ./test/main.tf .
+  cp ./test/*.tf .
 fi
 
 make BUILD_HARNESS_PATH=/build-harness PACKAGES_PREFER_HOST=true -f /build-harness/templates/Makefile.build-harness terraform/fmt
+
+if [[ "$HOST_REPO" == "cloudposse/github-action-auto-format" ]]; then
+  cp ./*.tf ./test/
+fi
 
 set -x
 
