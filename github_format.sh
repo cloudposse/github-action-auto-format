58a0b96389b160b8ef521fa36de973960903b673
dylanbannon <dylanbannon5@gmail.com>
2022-06-13 17:17:03 -0700

    Initial Auto-format functionality (#2)

---
+++ github_format.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -x
+
+echo "Copying workflows from cloudposse/.github repo"
+
+mkdir gha_tmp_dir
+cd gha_tmp_dir || exit 1
+git clone https://github.com/cloudposse/.github
+
+git config --global --add safe.directory /github/workspace
+
+## Deal with each workflow in cloudposse/.github/.github/workflows separately:
+
+# auto-context.yml
+# only Terraform repos should get this workflow
+if [ "$(git ls-files | grep .tf$ | wc -l)" -gt 0 ]; then
+  cp ./.github/.github/workflows/auto-context.yml ../.github/workflows/
+fi
+
+## auto-format.yml
+# if this is the cloudposse/github-action-auto-format repository, don't copy the version of auto-format.yml from cloudposse/.github during testing
+if [[ "$(git config --get remote.origin.url)" =~ cloudposse\/github-action-auto-format ]]; then
+  rm ./.github/.github/workflows/auto-format.yml
+else
+  git config --get remote.origin.url
+fi
+# all repos should get this workflow
+cp ./.github/.github/workflows/auto-format.yml ../.github/workflows/
+
+# auto-release.yml
+# all repos should get this workflow
+cp ./.github/.github/workflows/auto-release.yml ../.github/workflows/
+
+# ci-terraform.yml
+# only repos with Terraform files shuld get this workflow
+if [ "$(git ls-files | grep .tf$ | wc -l)" -gt 0 ]; then
+  cp ./.github/.github/workflows/auto-context.yml ../.github/workflows/
+fi
+
+## validate-codeowners.yml
+# all repos should get this workflow
+cp ./.github/.github/workflows/validate-codeowners.yml ../.github/workflows/
+
+cd ..
+rm -rf ./gha_tmp_dir
+
+git config --local user.name "${BOT_NAME}"
+git config --local user.email "${BOT_EMAIL}"
+git add ./.github/workflows/*
+
+# Don't try committing without any files staged. That returns a non-zero exit code.
+if ! git diff --staged --exit-code; then
+  git commit -m "Adding .github files"
+fi
---
+++ readme_format.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+make BUILD_HARNESS_PATH=/build-harness PACKAGES_PREFER_HOST=true -f /build-harness/templates/Makefile.build-harness pr/readme/host
+
+set -x
+
+# The `readme` build-harness target leaves some files lying around after it finishes. Since we might be running other
+# targets after this, in separate GitHub Action steps, we don't want to leave these files lying there, waiting to return
+# some permission error or something. That's why they get deleted, whether there were changes to `README.md` or not.
+
+output=$(git diff --name-only)
+if [ -n "$output" ]; then
+  echo "Changes detected. Pushing to the PR branch"
+  git config --global user.name "${BOT_NAME}"
+  git config --global user.email "${BOT_EMAIL}"
+  rm .build-harness
+  rm -r docs/
+  git add -A -- ':!'"${IGNORE_PATH}"''
+  git commit -m "Updating README.md"
+else
+  echo "No changes detected"
+  rm .build-harness
+  rm -r docs/
+fi
