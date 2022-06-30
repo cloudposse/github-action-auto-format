5ec3c3543d87c7cae4adb45b67621e80f4d0625b
Dylan Bannon <dylanbannon5@gmail.com>
2022-06-29 22:14:48 -0700

    setting pr labels independently for each function

--- scripts/context/pr_parameters.sh
+++ scripts/context/pr_parameters.sh
@@ -7,3 +7,7 @@ echo "::set-output name=commit-message::$(echo $PR_COMMIT_MESSAGE)"
 # PR body
 PR_BODY="This auto-generated PR updates this repo's `context.tf` file to be in sync with the newest `context.tf` found in the `cloudposse/terraform-null-label` repo"
 echo "::set-output name=pr-body::$(echo $PR_BODY)"
+
+# PR labels
+PR_LABELS="automated pr"
+echo "::set-output name=pr-labels::$(echo $PR_LABELS)"
--- scripts/readme/pr_parameters.sh
+++ scripts/readme/pr_parameters.sh
@@ -7,3 +7,7 @@ echo "::set-output name=commit-message::$(echo $PR_COMMIT_MESSAGE)"
 # PR body
 PR_BODY="This auto-generated PR updates `README.md` based on changes to `README.yaml`"
 echo "::set-output name=pr-body::$(echo $PR_BODY)"
+
+# PR labels
+PR_LABELS="automated pr,no-release"
+echo "::set-output name=pr-labels::$(echo $PR_LABELS)"
